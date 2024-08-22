64dc9d8644654e50d22cd8dd8b25da62a14ba6bf
NazarenaAranda06 <nazarenaaranda06@gmail.com>
2024-08-22 04:39:43 -0300

    solucion de propagacion

--- eventos.js
+++ eventos.js
@@ -1,3 +1,8 @@
+document.getElementById("boton-saluda").addEventListener("click", function() {
+    event.stopPropagation();
+    alert("Hola!");
+});
+
 document.getElementById("div-saluda").addEventListener("click", function() {
     alert("Hola! Soy el div.");
 });
